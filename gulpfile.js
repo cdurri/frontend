0646ebd4010b49e9f53bf8253e4fc1a9e767daf1
Cian <cian.durrigan@gmail.com>
2018-05-09 15:02:22 +0700

    Initial push to jw frontend repository

---
+++ gulpfile.js
@@ -0,0 +1,93 @@
+'use strict';
+
+var gulp = require('gulp'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+      sass = require('gulp-sass'),
+    minify = require('gulp-clean-css'),
+       del = require('del'),
+   tinypng = require('gulp-tinypng'),
+     babel = require('gulp-babel'),
+sourcemaps = require('gulp-sourcemaps');
+
+gulp.task("concatScripts", function() {
+  return gulp.src('sections/**/*.js')
+      .pipe(sourcemaps.init())
+      .pipe(babel({
+        presets: ['es2015']
+      }))
+      .on('error', function(e) {
+        console.log('>>> ERROR', e);
+        // emit here
+        this.emit('end');
+      })
+      .pipe(concat("main.js"))
+      .pipe(gulp.dest("src/js"));
+});
+
+gulp.task("minifyScripts", ["concatScripts"], function() {
+  return gulp.src("src/js/main.js")
+      .pipe(uglify())
+      .pipe(rename('main.min.js'))
+      .pipe(gulp.dest("assets/js"));
+});
+
+/*gulp.task('es6', ["concatScripts"] function() {
+  return gulp.src('src/js/main.js')
+      .pipe(sourcemaps.init())
+      .pipe(babel({
+        presets: ['es2015']
+      }))
+      .on('error', function(e) {
+        console.log('>>> ERROR', e);
+        // emit here
+        this.emit('end');
+      })
+      .pipe(gulp.dest('src/js'));
+});*/
+
+gulp.task('compileSass', function() {
+  return gulp.src("src/scss/main.scss")
+      .pipe(sass())
+      .pipe(gulp.dest('src/scss'));
+});
+
+gulp.task('minifyCss', function() {
+  return gulp.src("src/scss/main.css")
+      .pipe(minify())
+      .pipe(rename('main.min.css'))
+      .pipe(gulp.dest('assets/css'));
+});
+
+gulp.task('compressImages', function() {
+    gulp.src('src/img/*')
+        .pipe(tinypng('vHR3peFJDX89CSBttBzaN1ucq2fOP1_l'))
+        .pipe(gulp.dest('assets/img'));
+});
+
+gulp.task('moveMarkup', function() {
+  gulp.src('src/*.html')
+      .pipe(gulp.dest('assets'));
+})
+
+gulp.task('watchFiles', function() {
+  gulp.watch(['sections/**/*.scss', 'src/scss/**/*.scss'], ['compileSass']);
+  gulp.watch('sections/**/*.js', ['concatScripts']);
+});
+
+gulp.task('clean', function() {
+  del(['dist', 'assets/css/main.min.css', 'assets/js/main.min.js']);
+});
+
+gulp.task('build', ['minifyScripts', 'minifyCss', 'moveMarkup'], function() {
+  return gulp.src(["assets/css/main.min.css", "assets/js/main.min.js", 'assets/*.html',
+                   "assets/img/*"], { base: './' })
+              .pipe(gulp.dest('dist'));
+});
+
+gulp.task('serve', ['watchFiles']);
+
+gulp.task("default", ['clean'], function() {
+  gulp.start('build');
+});
---
+++ sections/age-gate/age-gate.js
@@ -0,0 +1,34 @@
+var day, month, year, age = 18, selectedDate, currentDate;
+
+const testOne = 1;
+
+var fullUrl = window.location.href;
+var endUrl = fullUrl.split('/').pop();
+
+if(endUrl == 'index.html' && location.hash !== 'loaded') {
+  location.hash = 'loaded';
+  window.location.assign('age-gate.html');
+}
+
+document.querySelector(".age-gate__enter").addEventListener("click", function( event ) {
+  event.preventDefault();
+
+    day = document.getElementById('age-gate__day');
+  month = document.getElementById('age-gate__month');
+   year = document.getElementById('age-gate__year');
+
+   selectedDate = new Date();
+   selectedDate.setFullYear( year.value, month.value - 1, day.value );
+   currentDate = new Date();
+   currentDate.setFullYear(currentDate.getFullYear() - age);
+
+   if( day.value == 'notselected' || month.value == 'notselected' || year.value == 'notselected' ) {
+     alert('Please enter your date of birth');
+     return false;
+   } else if ((currentDate - selectedDate) < 0) {
+     alert('Entry denied, currently you are not of legal drinking age');
+     return false;
+   } else {
+     window.location.assign("index.html#loaded");
+   }
+});
