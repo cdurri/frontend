6fbcf057fec8145a23dca97f615a736968584331
Cian <cian.durrigan@gmail.com>
2018-05-11 15:34:08 +0700

    Update gulpfile syntax form es5 to es6

---
+++ gulpfile.babel.js
@@ -0,0 +1,88 @@
+'use strict';
+
+/*var gulp = require('gulp'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+      sass = require('gulp-sass'),
+    minify = require('gulp-clean-css'),
+       del = require('del'),
+   tinypng = require('gulp-tinypng'),
+     babel = require('gulp-babel'),
+sourcemaps = require('gulp-sourcemaps');*/
+
+import gulp from 'gulp';
+import concat from 'gulp-concat';
+import uglify from 'gulp-uglify';
+import rename from 'gulp-rename';
+import sass from 'gulp-sass';
+import minify from 'gulp-clean-css';
+import del from 'del';
+import tinypng from 'gulp-tinypng';
+import babel from 'gulp-babel';
+import sourcemaps from 'gulp-sourcemaps';
+
+gulp.task("concatScripts", () => {
+  return gulp.src('sections/**/*.js')
+      .pipe(concat("main.js"))
+      .pipe(gulp.dest("src/js"));
+});
+
+gulp.task("minifyScripts", ["concatScripts"], () => {
+  return gulp.src("src/js/main.js")
+      .pipe(sourcemaps.init())
+      .pipe(babel())
+      .on('error', function(e) {
+        console.log('>>> ERROR', e);
+        // emit here
+        this.emit('end');
+      })
+      .pipe(uglify())
+      .pipe(rename('main.min.js'))
+      .pipe(gulp.dest("assets/js"));
+});
+
+gulp.task('compileSass', () => {
+  return gulp.src("src/scss/main.scss")
+      .pipe(sass())
+      .pipe(gulp.dest('src/scss'));
+});
+
+gulp.task('minifyCss', () => {
+  return gulp.src("src/scss/main.css")
+      .pipe(minify())
+      .pipe(rename('main.min.css'))
+      .pipe(gulp.dest('assets/css'));
+});
+
+gulp.task('compressImages', () => {
+    gulp.src('src/img/*')
+        .pipe(tinypng('vHR3peFJDX89CSBttBzaN1ucq2fOP1_l'))
+        .pipe(gulp.dest('assets/img'));
+});
+
+gulp.task('moveMarkup', () => {
+  gulp.src('src/*.html')
+      .pipe(gulp.dest('assets'));
+})
+
+gulp.task('watchFiles', () => {
+  gulp.watch(['sections/**/*.scss', 'src/scss/**/*.scss'], ['compileSass']);
+  gulp.watch('sections/**/*.js', ['concatScripts']);
+});
+
+gulp.task('clean', () => {
+  del(['dist', 'assets/css/main.min.css', 'assets/js/main.min.js']);
+});
+
+gulp.task('build', ['minifyScripts', 'minifyCss', 'moveMarkup'], () => {
+  return gulp.src(["assets/css/main.min.css", "assets/js/main.min.js", 'assets/*.html',
+                   "assets/img/*"], { base: './' })
+              .pipe(gulp.dest('dist'));
+});
+
+gulp.task('serve', ['watchFiles']);
+
+gulp.task("default", ['clean'], () => {
+  gulp.start('build');
+});
